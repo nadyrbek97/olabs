90ac42c8261466f4de47b49fd562daa2d40c48a9
nadyrbek97 <nadyrbek97@gmail.com>
2019-12-26 23:11:09 +0600

    finish Ford task and add jar file

--- lesson5/Nadyrbek/Model.java
+++ lesson5/Nadyrbek/Model.java
@@ -41,33 +41,22 @@ public class Model{
 
 	public void getAnswer(){
 		System.out.println("... Algorithm is creating now ...");
-		System.out.println("-----------------------------");
+		System.out.println("---------------------------------");
+		System.out.println("We have 10 unique numbers ... ");
 		System.out.println("0. D = 5. We already know it");
-		System.out.println("1. T = 0. Because 5 + 5 = 10, and second digit is 0 => T = 0. So we have +1 for next column => L + L + 1 = R");
+		System.out.println("1. T = 0. Because 5 + 5 = 10, and second digit is 0 => T = 0.");
+		System.out.println("          So we have +1 for next column => L + L + 1 = R");
 		System.out.println("2. E = 9. Because O + E = O, so E = 0 or 9. Since T = 0 => E = 9 => and this column must have extra 1");
 		System.out.println("3. R = 7. Becasue L + L + 1 = R. R must be an ODD number (1, 3, 5, 7, 9)");
 		System.out.println("          Since 5 + G = R. G != 0. R > 5. R = 7 or 9");
 		System.out.println("          Since E = 9   => R = 7");
 		System.out.println("4. G = 1. Since (extra)1 + O + 9 = O > 10 and it column before  5(D) + G + 1= 7(R) => G = 1");
-		System.out.println("5. L = 8. Since A + A = 9");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
-
+		System.out.println("5. L = 8. Since A + A + 1 = 9. A is even and that column must has 1(extra), so L + L + 1 > 10.");
+		System.out.println("          L must be even and equal to 7 or 17 and > 10 => L = 8");
+		System.out.println("6. A = 4. Since A + A + 1 = 9 < 10. A => 4");
+		System.out.println("7. N = 6. Since N + 7 = B > 10 and only [2,3,6]. N = 6");
+		System.out.println("8. B = 3. Since 6 + 7 = 13 => B = 3");
+		System.out.println("9. O = 2. Since only 2 left");
 	}
 
 }
